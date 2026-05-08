75ac2712cc38e614df096598d126518f569ceb0d
NICOLAS KAUÊ MARQUES BRIETZIG <nicolasbrietzig@univille.br>
2026-05-07 21:50:32 -0300

    minor changes

--- src/function_app.py
+++ src/function_app.py
@@ -6,27 +6,14 @@ app = func.FunctionApp()
 @app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def timer_trigger_aula02(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function executed.')
-    logging.warning('This is a warning message.')
-    logging.error('This is an error message.')
+    logging.info('MY NAME JAMES')
 
 @app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def timer_trigger_aula03(myTimer: func.TimerRequest) -> None:
-    
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function executed.')
+    logging.info('JAMES BOND')
 
 @app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def timer_trigger_aula04(myTimer: func.TimerRequest) -> None:
-    
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function executed.')
+    logging.info(':)')
