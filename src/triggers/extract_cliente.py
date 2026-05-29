c52b3bfb98f26756a53bda9ff9789c6d0f795b20
JORGE EDUARDO DUTRA <jorge.dutra@univille.br>
2026-05-28 21:13:22 -0300

    String Conexao com BD

--- src/triggers/extract_cliente.py
+++ src/triggers/extract_cliente.py
@@ -14,3 +14,35 @@ def extract_cliente(myTimer: func.TimerRequest) -> None:
     sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
 
     logging.info(f'Servidor: {sql_server}, \nDatabase: {sql_database}, \nUsuário: {sql_user}, \nSenha: {sql_pass}')
+    # Configura a string de conexão para o banco de dados SQL Server
+    conn_str = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={sql_server};"
+        f"DATABASE={sql_database};"
+        f"UID={sql_user};"
+        f"PWD={sql_pass};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+
+   
+    try:
+        # Estabelece a conexão com o banco de dados usando pyodbc
+        with pyodbc.connect(conn_str) as conn:
+            # Cria um cursor para executar a consulta   
+            cursor = conn.cursor()
+            
+            query = "select top 5 * from erp.pedido_item"
+
+            # Executa a consulta SQL
+            cursor.execute(query)
+
+            # Busca todos os resultados da consulta
+            rows = cursor.fetchall()
+
+            logging.info(rows)           
+
+    except Exception as e:
+        logging.error(f"Erro ao ler erp.pedido: {str(e)}")
+        raise
